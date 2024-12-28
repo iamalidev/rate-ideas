b467fb4b74955482be097b941c7ed1cdc925aed7
alisherotaboyev <iamotaboyev@gmail.com>
2024-12-28 22:00:48 +0500

    add netlify toml file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
-import { IconService } from './shared/services/icon.service';
+// import { IconService } from './shared/services/icon.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [RouterOutlet, MatButtonModule],
 })
-export class AppComponent implements OnInit {
-  iconsService = inject(IconService);
+export class AppComponent {
+  // iconsService = inject(IconService);
 
-  ngOnInit(): void {
-    this.iconsService.registerIcons();
-  }
+  // ngOnInit(): void {
+  //   this.iconsService.registerIcons();
+  // }
 }
